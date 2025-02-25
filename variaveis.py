e214d0d84ecbeb721bb9691763bf66d2a02fdbf9
Eduardo <eduardo25082002@gmail.com>
2025-02-24 21:36:57 -0300

    Primeiro

---
+++ variaveis.py
@@ -0,0 +1,13 @@
+# Declarações de Vars
+
+""" 
+Utilizo (=) para realizar a atribuição
+
+var = valor
+"""
+
+# Exemplo de atribuição
+nome = "Eduardo"
+idade = 22
+altura = 1.82
+e_estudante = True
