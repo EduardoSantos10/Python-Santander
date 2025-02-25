4c625d2ff40f0b5e72cdef9067206a598626b113
Eduardo <eduardo25082002@gmail.com>
2025-02-24 21:33:02 -0300

    Primeiro Commit

---
+++ ola_mundo.py
@@ -0,0 +1,18 @@
+# Comando do Olá Mundo
+print("Meu nome é Eduardo")
+
+"""
+Comentários em Python: # ou """"""
+"""
+
+# int (Inteiro)
+idade = 22
+anoNasc = 2002
+
+# float (Flutuantes)
+peso = 66.5
+altura = 1.82
+
+# string (Cadeia de Texto)
+nome = "Eduardo"
+mensagem = "Olá mundo"
